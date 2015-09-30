fabed7983f240836cbbb3aed16df461cbcfc46c4
Marie-Eve Gauthier <gauthier.marie.eve@gmail.com>
2015-09-30 18:08:50 +0000

    result with colors AND try and catch for validate the user word

--- exercise-4_synonyms_of_the_word_choosen_by_the_user.js
+++ exercise-4_synonyms_of_the_word_choosen_by_the_user.js
@@ -2,45 +2,48 @@
 var prompt = require("prompt");
 var request = require("request");
 var Promise = require("bluebird");
-
+var colors = require("colors");
 
 //need to promisify that two libraries
 prompt = Promise.promisifyAll(prompt);
 request = Promise.promisifyAll(request);
 
-//prompt the user for an english word DON'T FORGET TO START the prompt!!!
+//prompt the user for an english word and DON'T FORGET TO START the prompt!!!
 prompt.start();
-prompt.getAsync('an_english_word').then(
+
+prompt.getAsync("an_english_word").then(
     function(userResponse){
-/*use the result of this prompt to request the Big Huge Thesaurus 
-for this word 
-*/
-    return request.getAsync("http://words.bighugelabs.com/api/2/fae5488a3aae2345462928e178ee6a29/"+ userResponse.an_english_word + "/json").then(
-        function(dictResponse) {
-//with this result, I have to find specifically the synonyms
-            var word = dictResponse[0].body;
-            var synonyms = JSON.parse(word);
-            
-//then console.log the result
-            if (synonyms.noun && synonyms.noun.syn) {
-                console.log("Noun synonyms:");
-                console.log(synonyms.noun.syn.join(", "));
-            }
-            if (synonyms.verb && synonyms.verb.syn) {
-                console.log("Verb synonyms:");
-                console.log(synonyms.verb.syn.join(", "));
-            } 
-            if (synonyms.adjective && synonyms.adjective.syn) {
-                console.log("Adjective synonyms:");
-                console.log(synonyms.adjective.syn.join(", "));
-            }
-            if (synonyms.adverb && synonyms.adverb.syn) {
-                console.log("Adverb synonyms:");
-                console.log(synonyms.adverb.syn.join(", "));
-            }
-            
+//Using English Word, Query the Dictionary
+        request.getAsync("http://words.bighugelabs.com/api/2/fae5488a3aae2345462928e178ee6a29/"+ userResponse.an_english_word + "/json").then(
+            function(dictResponse) {
+//Catch the error if it's not a valid word
+                try {
+                    var synonyms = JSON.parse(dictResponse[0].body);
+    //then console.log the result
+                    if (synonyms.noun && synonyms.noun.syn) {
+                        console.log("Noun synonyms:".underline.bold.bgMagenta);
+                        console.log(synonyms.noun.syn.join(", ").magenta);
+                    }
+                    if (synonyms.verb && synonyms.verb.syn) {
+                        console.log("Verb synonyms:".underline.bold.bgBlue);
+                        console.log(synonyms.verb.syn.join(", ").blue);
+                    } 
+                    if (synonyms.adjective && synonyms.adjective.syn) {
+                        console.log("Adjective synonyms:".underline.bold.bgGreen);
+                        console.log(synonyms.adjective.syn.join(", ").green);
+                    }
+                    if (synonyms.adverb && synonyms.adverb.syn) {
+                        console.log("Adverb synonyms:".underline.bold.bgCyan);
+                        console.log(synonyms.adverb.syn.join(", ").cyan);
+                    }                    
+                        
+                } catch (SyntaxError) {
+                    console.log("Please re-run this program because: " + SyntaxError.message);
+                }
+            });
+      
         });
-    });
+
 
 
 
